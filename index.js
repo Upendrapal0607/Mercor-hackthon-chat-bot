57c1de673a2128674c82d64812172f239a953b81
Upendra Pal <up06072000@gmail.com>
2023-09-03 19:22:48 +0530

    Mercor Hackathon

---
+++ index.js
@@ -0,0 +1,99 @@
+const ulTage = document.querySelector("ul");
+const sendChatbtn = document.querySelector(".input-btn");
+const chatinput = document.querySelector(".input-text");
+const UlTagClassName = ulTage.classList[0];
+const ptag = document.querySelector(".hello");
+const burger = document.querySelector(".burger");
+const deletebtn = document.querySelector(".delete");
+// console.log(sendChatbtn)
+let userMessage;
+const createChatLi = (message, className) => {
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", className);
+  let chatContent =
+    className == "outgoing"
+      ? `
+    <span>
+    <i class="fa-regular fa-user"></i>
+    
+    </span>
+    <p></p>
+`
+      : ` 
+<span>
+<i class="fa-brands fa-discord"></i></span>
+
+<p></p>
+ `;
+
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent=message
+  return chatLi;
+};
+
+// <i class="fa-solid fa-trash delete"></i>
+
+const getDataFromApi = (incommitMessage) => {
+  const messageElement = incommitMessage.querySelector("p");
+
+  let API_KEY = "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";
+  let URL = "https://api.openai.com/v1/chat/completions";
+  console.log("userMessage", userMessage);
+  const postDataDetails = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: userMessage,
+        },
+      ],
+    }),
+  };
+  fetch(URL, postDataDetails)
+    .then((res) => res.json())
+    .then((data) => {
+      //   console.log(data.choices[0].message.content);
+      messageElement.textContent = data.choices[0].message.content;
+    })
+    .catch((error) => {
+      messageElement.textContent =
+        "opps! something went wrong please try again.";
+    })
+    .finally(() => {
+      ulTage.scrollTo(0, ulTage.scrollHeight);
+    });
+};
+
+const handleMessage = () => {
+  ptag?.remove();
+  ulTage.classList.remove("chatbox-start");
+  ulTage.classList.add("chatbox");
+
+  userMessage = chatinput.value?.trim();
+  if (!userMessage) return;
+
+  const LiElement = createChatLi(userMessage, "outgoing");
+  ulTage.appendChild(LiElement);
+  ulTage.scrollTo(0, ulTage.scrollHeight);
+
+  setTimeout(() => {
+    let outputMessage = createChatLi("Think...", "incomming");
+    ulTage.appendChild(outputMessage);
+    ulTage.scrollTo(0, ulTage.scrollHeight);
+    // setTimeout(()=>{
+    getDataFromApi(outputMessage);
+    // },500)
+  }, 1000);
+
+  // console.log(userMessage)
+
+  chatinput.value = "";
+};
+
+sendChatbtn.addEventListener("click", handleMessage);

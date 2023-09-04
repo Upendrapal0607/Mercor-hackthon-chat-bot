8c0770ee3f4443ecf911a680853e6ef6fe0c4fa3
Upendra Pal <up06072000@gmail.com>
2023-09-04 23:19:00 +0530

    Done

--- Secret.mjs
+++ Secret.mjs
@@ -1,4 +1,4 @@
- export const API_KEY="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"
+ export const API_KEY="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"
  export const URL="https://api.openai.com/v1/chat/completions"
 
 //  import { API_KEY, URL } from "./Secret.mjs";
